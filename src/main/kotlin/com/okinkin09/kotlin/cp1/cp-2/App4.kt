3e7a120db78c565bd1cd2721ca5e9a730e815b00
Brooklyn Kairupan <brokinignacio3@gmail.com>
2023-03-16 10:41:16 +0800

    cp2-3

--- src/main/kotlin/com/okinkin09/kotlin/cp1/cp-2/App4.kt
+++ src/main/kotlin/com/okinkin09/kotlin/cp1/cp-2/App4.kt
@@ -1,30 +1,9 @@
 package com.okinkin09.kotlin.cp1.`cp-2`
 
-//fun main() {
-//    println("masukan angka: ")
-//    val n = readLine()?.toInt() ?: 0
-//    val m = readLine()?.toInt() ?: 0
-//    var num = n
-//    var seq = mutableListOf<Int>()
-//
-//    while (num > 1) {
-//        seq.add(num)
-//        if (num % m == 0) {
-//            num = num / m
-//        } else {
-//            println("boring!")
-//            break
-//        }
-//    }
-//    if (num == 1) {
-//        seq.add(num)
-//        println(seq.joinToString(","))
-//    }
-//}
-
 fun main() {
-    var n = 16
-    var m = 2
+    println("masukan angka: ")
+    val n = readLine()?.toInt() ?: 0
+    val m = readLine()?.toInt() ?: 0
     var num = n
     var seq = mutableListOf<Int>()
 
@@ -42,6 +21,8 @@ fun main() {
         println(seq.joinToString(","))
     }
 }
+
+
 
 
 
