a2f93a1897c52f1c6de99050d0dee5765ee7df9b
Brooklyn Kairupan <brokinignacio3@gmail.com>
2023-04-13 09:54:05 +0800

    cp-4 / 2

--- src/main/kotlin/com/okinkin09/kotlin/cp1/cp-4/App2.kt
+++ src/main/kotlin/com/okinkin09/kotlin/cp1/cp-4/App2.kt
@@ -1,20 +1,29 @@
 package com.okinkin09.kotlin.cp1.`cp-4`
 
-fun main() {
-    val m = readLine()!!.toInt()
-    repeat(m) {
-        val num = readLine()!!.toInt()
-        val digits = num.toString().map { it.toString().toInt() }
-        var count = 0
-        var count1 = 0
-        for (digit in digits) {
-            if (digit != 0 && num % digit == 0) {
-                count++
-            }
-            if (digit != 0 && num % digit != 0) {
-                count1++
-            }
-        }
-        println("$count $count1")
+import java.util.*
+
+fun getNumBits(n: Int): Int {
+    var count = 0
+    var num = n
+    while (num != 0) {
+        count += num and 1
+        num = num shr 1
     }
+    return count
+}
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val T = scanner.nextInt()
+    repeat(T) {
+        val n = scanner.nextInt()
+        val x1 = getNumBits(n)
+        var x2 = 0
+        var num = n
+        while (num != 0) {
+            x2 += getNumBits(num % 10)
+            num /= 10
+        }
+        println("$x1 $x2")
+        }
 }
