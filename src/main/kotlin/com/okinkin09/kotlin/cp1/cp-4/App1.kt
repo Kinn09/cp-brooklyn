0f61719b90ceb8cdb3d514fd5a1a0257a8413f6e
Brooklyn Kairupan <brokinignacio3@gmail.com>
2023-04-13 09:28:30 +0800

    cp-4 / 2

--- src/main/kotlin/com/okinkin09/kotlin/cp1/cp-4/App1.kt
+++ src/main/kotlin/com/okinkin09/kotlin/cp1/cp-4/App1.kt
@@ -1,45 +1,5 @@
 package com.okinkin09.kotlin.cp1.`cp-4`
 
-//fun isPrime(num: Int, i: Int = 2): Boolean {
-//    if (num <= 2) {
-//        return num == 2
-//    }
-//    if (num % i == 0) {
-//        return false
-//    }
-//    if (i * i > num) {
-//        return true
-//    }
-//    return isPrime(num, i + 1)
-//}
-//
-//fun reverse(num: Int): Int {
-//    var n = num
-//    var reversed = 0
-//    while (n > 0) {
-//        reversed = reversed * 10 + n % 10
-//        n /= 10
-//    }
-//    return reversed
-//}
-//
-//fun isEmirp(num: Int): Boolean {
-//    val reversedNum = reverse(num)
-//    return num != reversedNum && isPrime(num) && isPrime(reversedNum)
-//}
-//
-//fun main() {
-//    for (i in 1..100) {
-//        if (isEmirp(i)) {
-//            println("case #$i: emirp number ")
-//        } else if (isPrime(i)) {
-//            println("case #$i: prime number ")
-//        } else {
-//            println("case #$i: not a prime number")
-//        }
-//    }
-//}
-
 fun isPrime(num: Int, i: Int = 2): Boolean {
     if (num <= 2) {
         return num == 2
